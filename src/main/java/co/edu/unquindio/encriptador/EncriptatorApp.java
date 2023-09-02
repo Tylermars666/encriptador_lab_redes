af943c26184f59bae8af7321d47ebcc64dfcb612
Omar <omar.piedrahitat@gmail.com>
2023-09-01 19:18:29 -0500

    xml configuration

--- src/main/java/co/edu/unquindio/encriptador/EncriptatorApp.java
+++ src/main/java/co/edu/unquindio/encriptador/EncriptatorApp.java
@@ -11,8 +11,7 @@ public class EncriptatorApp extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(EncriptatorApp.class.getResource("view/principal-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
         stage.setScene(scene);
         stage.show();
     }
